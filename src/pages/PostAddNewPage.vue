fc6a50076496243eb1f4138d43ece39aca2b7580
KristijanJ <kristijan.j92@gmail.com>
2022-05-14 13:41:33 +0200

    add new wip

--- src/pages/PostAddNewPage.vue
+++ src/pages/PostAddNewPage.vue
@@ -1,12 +1,45 @@
 <template>
     <div class="container">
-        hey
+        <div class="form-group">
+            <label for="post-title">Post title</label>
+            <input id="post-title" type="text" name="post-title">
+        </div>
+
+        <div class="form-group">
+            <label for="post-text">Post text</label>
+            <textarea
+                id="post-text"
+                name="post-text"
+                cols="30"
+                rows="10"
+            />
+        </div>
+
+        <div class="form-group form-group-center">
+            <button class="btn" @click="handleAddNewPost">
+                Add new post
+            </button>
+        </div>
     </div>
 </template>
 
 <script>
+import { getForumBoards } from '../assets/mocks/forumBoardsMockData';
+
 export default {
-    name: 'PosAddNewPage'
+    name: 'PosAddNewPage',
+    data () {
+        return {
+            forumBoards: getForumBoards()
+        };
+    },
+    methods: {
+        handleAddNewPost () {
+            const boardId = this.$route.params.id;
+            const currentBoard = this.forumBoards[boardId];
+            console.log(currentBoard);
+        }
+    }
 };
 </script>
 
